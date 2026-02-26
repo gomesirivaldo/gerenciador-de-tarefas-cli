49d0cc2f4eb08594e9041621be1b939307290acd
Taki <irivaldogomes34@gmail.com>
2026-02-26 15:26:53 -0300

    Adiciona menu inicial interativo no CLI

---
+++ main.py
@@ -0,0 +1,8 @@
+print("=== TASK MANAGER ===")
+print("1 - Adicionar tarefa")
+print("2 - Listar tarefas")
+print("3 - Sair")
+
+opcao = input("Escolha uma opção: ")
+
+print("Você escolheu:", opcao)
